c2f03ee56a445faf9d0a8b1c11ce73f6b194a3d4
Jieun Lee <najee0610@naver.com>
2024-04-22 12:20:22 +0900

    ✨ feat: tailwind css 설치 및 테마컬러 추가

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mywhite: "#FFFFFF",
+        mymint: "#A3FFD6",
+        mypurple: "#8576FF",
+        myblue: "#7BC9FF",
+      },
+    },
+  },
+  plugins: [],
+};
