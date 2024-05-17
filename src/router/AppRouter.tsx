b38dd4a0999d23356ad91e1bbc45fa7a89af6da5
Jieun Lee <najee0610@naver.com>
2024-05-17 18:23:34 +0900

    ✨ feat: 페이지 당 router 추가

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,13 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import PhotoUpload from "../pages/PhotoUpload/PhotoUpload";
 import KaKaoLogin from "../pages/Login/KaKaoLoginForm";
+import Header from "../components/Header/header";
+import RegisterDoctorMedi from "../pages/RegisterDoctorMedi/RegisterDoctorMedi";
+import MediInfo from "../pages/MediInfo/MediInfo";
+import WarnMedi from "../pages/WarnMedi/WarnMedi";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Header title="이약머약" />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/upload" element={<PhotoUpload />} />
+        <Route path="/register" element={<RegisterDoctorMedi />} />
+        <Route path="/mediinfo" element={<MediInfo />} />
+        <Route path="/warnmedi" element={<WarnMedi />} />
+
         <Route path="/kakao-login" element={<KaKaoLogin />} />
         {/* <Route path="/login/oauth2/code/kakao" element={<KaKaoRedirectHandler />} /> */}
       </Routes>
