299d6f93ab813401df06d05f351936c5fb8ff692
Jieun Lee <najee0610@naver.com>
2024-05-26 18:15:49 +0900

    ✨ feat: 복약정보 페이지 개별약 목록 수정

--- src/pages/MediInfo/components/MediCard.tsx
+++ src/pages/MediInfo/components/MediCard.tsx
@@ -5,32 +5,10 @@ type Props = {
     name: string;
     start_date: string;
     end_date: string;
-    dose_time: string;
-    meal_time: number;
   };
 };
 
-const doseTimeMapping: { [key: string]: string } = {
-  M: "아침",
-  L: "점심",
-  D: "저녁",
-  N: "취침 전",
-};
-
-const mealTimeMapping: { [key: number]: string } = {
-  0: "식후 30분",
-  1: "식후 즉시",
-  2: "식전 30분",
-  3: "식전 즉시",
-};
-
 const MediCard: FC<Props> = ({ medicine }) => {
-  const doseTimeDisplay = medicine.dose_time
-    .split(";")
-    .map((time) => doseTimeMapping[time])
-    .join(", ");
-  const mealTimeDisplay = mealTimeMapping[medicine.meal_time];
-
   return (
     <div className="h-3/12 flex w-1/4 flex-col gap-1 bg-mywhite p-7">
       <h1 className="h-32 w-full pb-3 pl-2 pt-2 text-2xl">{medicine.name}</h1>
@@ -38,10 +16,6 @@ const MediCard: FC<Props> = ({ medicine }) => {
       <p>{medicine.start_date}</p>
       <h6>복용 종료 :</h6>
       <p>{medicine.end_date}</p>
-      <h6>복용 시간 :</h6>
-      <p>{doseTimeDisplay}</p>
-      <h6>식사 시간 :</h6>
-      <p>{mealTimeDisplay}</p>
     </div>
   );
 };
