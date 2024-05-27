2e67c5b7f4a5928b85c4dc3689d9e4451bb3169c
Jieun Lee <najee0610@naver.com>
2024-05-28 02:29:20 +0900

    ✨ feat: 세부 복약정보 페이지 퍼블리싱

---
+++ src/pages/DetailMediInfo/DetailMediInfo.tsx
@@ -0,0 +1,13 @@
+import DetailMediInfoContents from "./components/DetailMediInfoContents";
+
+type Props = {};
+
+const DetailMediInfo = (props: Props) => {
+  return (
+    <div>
+      <DetailMediInfoContents />
+    </div>
+  );
+};
+
+export default DetailMediInfo;
